0dbbd76cef01833f5c78fb4dcb86623d65ccc15b
Siddhesh Khedekar <siddheshkhedekar@hotmail.com>
2018-10-29 09:59:39 +0530

    update changes after reveiw

--- agents/.ipynb_checkpoints/agent-checkpoint.py
+++ agents/.ipynb_checkpoints/agent-checkpoint.py
@@ -89,7 +89,7 @@ class DDPG():
 
         # Algorithm parameters
         self.gamma = 0.99  # discount factor
-        self.tau = 0.001  # for soft update of target parameters
+        self.tau = 0.0001  # for soft update of target parameters
 
     def reset_episode(self):
         self.noise.reset()
